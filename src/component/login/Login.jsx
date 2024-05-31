f3574316f65cb2d8b04511fdf06af621e4f4ec70
jhraju <jhraju890@gmail.com>
2024-05-31 09:45:20 +0600

    added signout popup

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup  } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut  } from "firebase/auth";
 import app from '../firebase/firebase.init';
 
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     
     const handleGoogleSignIn = () =>{
      signInWithPopup(auth, provider)
      .then(result =>{
-        const user = result.user;
-        console.log(user);
+        const loggedInuser = result.user;
+        console.log(loggedInuser);
+        setUser(loggedInuser);
      })
      .catch(error =>{
         console.log('error', error.message)
      })
     }
 
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(result =>{
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
 
     return (
         <div>
             <button onClick={handleGoogleSignIn}>login</button>
+            <button onClick={handleSignOut}>signout</button>
+           { user &&  <div>
+                <h3>User:{user.displayName}</h3>
+            </div>}
         </div>
     );
 };
